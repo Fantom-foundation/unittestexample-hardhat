3d5a41a5bede16cb146f2b34cc2d8ad03cd21dc3
prolingua <prolingua.geo@gmail.com>
2022-11-30 11:45:31 +0000

    Create GoldToken.js

---
+++ test/GoldToken.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const etherToWei = (n) => {
+  return ethers.utils.parseEther(n.toString());
+};
+const TOTAL_SUPPLY = etherToWei(1000_000);
+
+describe("GoldToken", () => {
+  let goldToken;
+  let owner, otherAccount;
+
+  beforeEach(async () => {
+      [owner, otherAccount, otherAccount2] = await ethers.getSigners();
+      const GoldToken = await ethers.getContractFactory('GoldToken');
+      goldToken = await GoldToken.deploy(TOTAL_SUPPLY.toString());
+  });
+
+  describe("Deployment",  () => {
+      it("Should has total supply of 1000_000 GoldTokens", async () => {
+          const totalSupply = await goldToken.totalSupply();
+          expect(totalSupply.toString()).to.equal(TOTAL_SUPPLY.toString());
+      });
+  });
+  describe("Events", () => {
+      it("Should emit Transfer event with correct arguments", async () => {
+          await expect(goldToken.transfer(otherAccount.address, 1000))
+          .to.emit(goldToken, "Transfer").withArgs(owner.address, otherAccount.address, 1000);
+      });
+  });
+
+  describe("Reverts", () => {
+      it("Should cause revert 'ERC20: transfer amount exceeds balance'", async () => {
+          await expect(goldToken.connect(otherAccount).transfer(otherAccount2.address, 1000))
+          .to.be.revertedWith("ERC20: transfer amount exceeds balance");
+      });
+  });
+})
